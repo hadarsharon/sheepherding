86fd270ede5c138cbfe2b4d62d23b20484585ac4
hunor1 <21153756+hunor1@users.noreply.github.com>
2024-02-14 21:07:44 +0000

    Add camera changes

---
+++ Assets/Scripts/CameraZoomController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraZoomController : MonoBehaviour
+{
+    [SerializeField]
+    public GameObject dog;
+    [SerializeField]
+    public float YPosition = 8f;
+
+    private Vector3 offset;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        offset = transform.position - dog.transform.position;
+    }
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        transform.position = dog.transform.position - offset;
+
+        transform.position = new Vector3(transform.position.x, YPosition, transform.position.z);
+    }
+}
