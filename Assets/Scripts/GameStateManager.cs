2de77301b902d0fb91d72bfe1c9d84137eddfc9e
Zoe Sklair <zoe.sklair1@virginmedia.com>
2024-02-15 18:05:02 +0000

    add skeleton code for level set-up

--- Assets/Scripts/GameStateManager.cs
+++ Assets/Scripts/GameStateManager.cs
@@ -9,6 +9,8 @@ public class GameStateManager : MonoBehaviour
     public GameStates CurrentGameState => currentGameState;
 
     private GameStates currentGameState;
+
+    private List<LevelData> levelDatas;
     
     //should match scene IDs in Build Settings
     public enum GameStates
@@ -37,7 +39,14 @@ public class GameStateManager : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        //game loads into Main Menu
         currentGameState = GameStates.MainMenu;
+
+        //load up data for levels
+        levelDatas = new List<LevelData>();
+        LevelData levelData1 = new LevelData(5, 10, 0);
+        levelDatas.Add(levelData1);
+
     }
 
     public void ChangeGameState(GameStates newGameState)
@@ -45,12 +54,39 @@ public class GameStateManager : MonoBehaviour
         if(newGameState != currentGameState)
         {
             MoveScene((int)newGameState);
+
+            currentGameState = newGameState;
+
+            if((int)currentGameState > 2) //state is a game level, set up required
+            {
+                SetUpGameLevel();
+            }
         }
     }
-
+    
     private void MoveScene(int sceneId)
     {
         SceneManager.LoadScene(sceneId);
+    }
+
+    private void SetUpGameLevel()
+    {
+        //add sheep to scene and set their speed values
+        
+    }
+    ///////Level Data///////
+    public class LevelData
+    {
+        public int noOfSheep;
+        public float sheepSpeedBase;
+        public float sheepSpeedRange;
+
+        public LevelData(int noOfSheep, float sheepSpeedBase, float sheepSpeedRange)
+        {
+            this.noOfSheep = noOfSheep;
+            this.sheepSpeedBase = sheepSpeedBase;
+            this.sheepSpeedRange = sheepSpeedRange;
+        }
     }
     
 }
